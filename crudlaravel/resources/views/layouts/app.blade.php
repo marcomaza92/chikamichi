fb6202a1665b67082b1e9a47fad03a626d780ef2
Marco Maza <marcoagustinmaza@gmail.com>
2017-12-14 23:22:15 -0300

    Update views with Bootstrap layout

--- crudlaravel/resources/views/layouts/app.blade.php
+++ crudlaravel/resources/views/layouts/app.blade.php
@@ -13,49 +13,47 @@
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 </head>
-<body>
-  <div class="container.fluid" id="app"> <!-- Why does it have an 'id="app"' -->
-    <header class="row">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{ url('/tnxs') }}">{{ config('app.name', 'Login With CRUD') }}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+<body id="app"> <!-- Why does it have an 'id="app"' -->
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="{{ url('/tnxs') }}">{{ config('app.name', 'Login With CRUD') }}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <!-- <li class="nav-item active">
-              <a class="nav-link" href="{{ url('/tnxs') }}">Home <span class="sr-only">(current)</span></a>
-            </li> -->
-            @guest
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">Register</a>
-            </li>
-            @else
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hello, {{ Auth::user()->name }}</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-              </div>
-            </li>
-            @endguest
-          </ul>
-        </div>
-      </nav>
-    </header>
-    <section class="row">
-      @yield('content')
-    </section>
-    <footer>
-      <p><a href="https://hipperstudio.com/" target="_blank">Hipper Studio - 2017</a></p>
-    </footer>
-  </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="{{ url('/tnxs') }}">Home <span class="sr-only">(current)</span></a>
+          </li> -->
+          @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+          @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hello, {{ Auth::user()->name }}</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+              </form>
+            </div>
+          </li>
+          @endguest
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <section class="container-fluid">
+    @yield('content')
+  </section>
+  <footer>
+    <p class="text-center"><a href="https://hipperstudio.com/" target="_blank">Hipper Studio - 2017</a></p>
+  </footer>
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}"></script>
--- crudlaravel/resources/views/tnxs/index.blade.php
+++ crudlaravel/resources/views/tnxs/index.blade.php
@@ -6,15 +6,13 @@
 
     @if(Auth::guest())
 
-      <div class="row">
-          <div class="col-lg-12">
-            <h1 class="text-center">Transactions CRUD</h1>
-          </div>
+      <div class="text-center">
+        <h1>Transactions CRUD</h1>
       </div>
 
-      <div class="row text-center">
-          <p>You are a <strong>Guest</strong>, please login below</p>
-          <a class="btn btn-success" href="{{ route('login') }}">Go to Login</a>
+      <div class="text-center">
+        <p>You are a <strong>Guest</strong>, please login below</p>
+        <a class="btn btn-success" href="{{ route('login') }}">Go to Login</a>
       </div>
 
     @endif
@@ -23,46 +21,44 @@
 
     @if(Auth::check())
 
-      <div class="row">
-          <div class="col-lg-12">
-            <h1 class="text-center">Transactions CRUD</h1>
-          </div>
+      <div>
+        <h1>Transactions CRUD</h1>
       </div>
 
       <div class="pull-right">
-          <a class="btn btn-success" href="{{ route('tnxs.create') }}">Create New Transaction</a>
+        <a class="btn btn-success" href="{{ route('tnxs.create') }}">Create New Transaction</a>
       </div>
 
       @if ($message = Session::get('success'))
-          <div class="alert alert-success">
-              <p>{{ $message }}</p>
-          </div>
+      <div class="alert alert-success">
+        <p>{{ $message }}</p>
+      </div>
       @endif
 
       <table class="table table-bordered">
-          <tr>
-              <th>No</th>
-              <th>Type</th>
-              <th>Origin</th>
-              <th>Description</th>
-              <th>Amount</th>
-              <th width="280px">Action</th>
-          </tr>
+        <tr>
+          <th>No</th>
+          <th>Type</th>
+          <th>Origin</th>
+          <th>Description</th>
+          <th>Amount</th>
+          <th width="280px">Action</th>
+        </tr>
       @foreach ($tnxs as $key => $tnx)
-      <tr>
+        <tr>
           <td>{{ ++$i }}</td>
           <td>{{ $tnx->type }}</td>
           <td>{{ $tnx->origin }}</td>
           <td>{{ $tnx->description }}</td>
           <td>{{ $tnx->amount }}</td>
           <td>
-              <a class="btn btn-info" href="{{ route('tnxs.show',$tnx->id) }}">Show</a>
-              <a class="btn btn-primary" href="{{ route('tnxs.edit',$tnx->id) }}">Edit</a>
-              {!! Form::open(['method' => 'DELETE','route' => ['tnxs.destroy', $tnx->id],'style'=>'display:inline']) !!}
-              {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-              {!! Form::close() !!}
+            <a class="btn btn-info" href="{{ route('tnxs.show',$tnx->id) }}">Show</a>
+            <a class="btn btn-primary" href="{{ route('tnxs.edit',$tnx->id) }}">Edit</a>
+            {!! Form::open(['method' => 'DELETE','route' => ['tnxs.destroy', $tnx->id],'style'=>'display:inline']) !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+            {!! Form::close() !!}
           </td>
-      </tr>
+        </tr>
       @endforeach
       </table>
 
